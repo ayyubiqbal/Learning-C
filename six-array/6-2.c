5e39b6a1e4e7a4ecb21306730b4c9c244e0e83d0
unknown <abuayubiqbal@gmail.com>
2022-05-30 11:06:25 +0600

    array in c

---
+++ six-array/6-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int array[5] = {10,20,30,40,50};
+    printf("First Element of array %d\n", array[0]);
+    printf("Second Element of array %d\n", array[1]);
+}
